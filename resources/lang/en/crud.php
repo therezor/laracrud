5c7f21c90c5a37121d7c1655bc5de3a0d254bd36
Roman Nagirnyi <zmrezor@gmail.com>
2019-04-12 13:59:28 +0300

    Add english translations

---
+++ resources/lang/en/crud.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    'list'  => 'List',
+    'total' => 'Total',
+
+
+    'filters' => 'Filters',
+    'filter'  => 'Filter',
+
+    'actions' => 'Actions',
+
+    'created' => 'Item created.',
+    'updated' => 'Item updated.',
+    'deleted' => 'Item deleted.',
+
+    'empty_result' => 'Empty...',
+
+    'show'    => 'Show',
+    'create'  => 'Create',
+    'edit'    => 'Edit',
+    'update'  => 'Update',
+    'destroy' => 'Delete',
+
+    'cancel' => 'Cancel',
+
+
+    'item'    => 'Item',
+    'value'   => 'Value',
+    'save'    => 'Save',
+    'saved'   => 'Saved',
+    'add'     => 'Add',
+    'added'   => 'Added',
+    'preview' => 'Preview',
+    'apply'   => 'Apply',
+
+    'confirmation' => 'Do you confirm the action?',
+
+    'action_completed' => 'Action completed.',
+];
